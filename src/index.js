c1f7af6e36cc07faefefdeac5911e2a87b76aded
djjones11 <33580761+djjones11@users.noreply.github.com>
2025-04-09 19:46:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";  // Global styles for your app
+import App from "./App";
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
