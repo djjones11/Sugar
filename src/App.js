9083c338672deb1e747b96e1f6432506e4f332f8
djjones11 <33580761+djjones11@users.noreply.github.com>
2025-04-09 19:45:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from "react";
+import SugarCubeVisualizer from "./components/SugarCubeVisualizer";
 
+function App() {
+  return (
+    <div className="App">
+      <SugarCubeVisualizer />
+    </div>
+  );
+}
+
+export default App;
